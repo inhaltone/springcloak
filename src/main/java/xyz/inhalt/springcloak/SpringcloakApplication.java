43ef19b82e027ada2c8871dbd8c0320d4a5878e6
inhaltone <inhalt.xyz@gmail.com>
2025-09-21 10:49:38 +0300

    first commit

---
+++ src/main/java/xyz/inhalt/springcloak/SpringcloakApplication.java
@@ -0,0 +1,13 @@
+package xyz.inhalt.springcloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/inhalt/springcloak/SpringcloakApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.inhalt.springcloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
